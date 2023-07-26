eeb6fe124d9d4471508bc772dd8db447a1821d1e
Michael Challinor <92083194+c026128l@users.noreply.github.com>
2023-07-26 22:09:35 +0100

    added SFML

--- Platforming/Platforming.cpp
+++ Platforming/Platforming.cpp
@@ -6,7 +6,7 @@
 int main()
 {
     std::cout << "Hello World!\n"; 
-        std:: << cout << "plzwork";
+    std::cout << "plzwork";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
